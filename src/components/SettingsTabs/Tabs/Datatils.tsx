26fd37e9a435b9c281a20698f65be3dccd2bd8bb
Emerson Mendonça <emerson_10_s@hotmail.com>
2024-05-19 15:44:10 -0300

    input de upload de arquivo

--- src/components/SettingsTabs/Tabs/Datatils.tsx
+++ src/components/SettingsTabs/Tabs/Datatils.tsx
@@ -1,6 +1,7 @@
 import * as Input from '@/components/Input'
 import { Separator } from '@/components/Separator'
 import { Mail } from 'lucide-react'
+import * as FileInput from '@/components/Form/FileInput'
 
 export function Details() {
   return (
@@ -33,7 +34,7 @@ export function Details() {
         id="settings"
         className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200"
       >
-        <div className="grid-cols-form grid gap-3">
+        <div className="grid grid-cols-form gap-3">
           <label
             htmlFor="firstName"
             className="text-sm font-medium text-zinc-700"
@@ -51,7 +52,7 @@ export function Details() {
           </div>
         </div>
 
-        <div className="grid-cols-form grid gap-3 pt-5">
+        <div className="grid grid-cols-form gap-3 pt-5">
           <label htmlFor="email" className="text-sm font-medium text-zinc-700">
             Email address
           </label>
@@ -68,18 +69,25 @@ export function Details() {
           </Input.Root>
         </div>
 
-        <div className="grid-cols-form grid gap-3 pt-5">
-          <label htmlFor="photo" className="text-sm font-medium text-zinc-700">
+        <div className="grid grid-cols-form gap-3 pt-5">
+          <label
+            htmlFor="photoFile"
+            className="text-sm font-medium text-zinc-700"
+          >
             your photo
             <span className="mt-0.5 block text-sm font-normal text-zinc-500">
               This will be displayed on your profile
             </span>
           </label>
 
-          <div></div>
+          <FileInput.Root className="flex items-start  gap-5">
+            <FileInput.ImagePreview />
+            <FileInput.Trigger />
+            <FileInput.Control />
+          </FileInput.Root>
         </div>
 
-        <div className="grid-cols-form grid gap-3 pt-5">
+        <div className="grid grid-cols-form gap-3 pt-5">
           <label htmlFor="role" className="text-sm font-medium text-zinc-700">
             Role
           </label>
@@ -89,7 +97,7 @@ export function Details() {
           </Input.Root>
         </div>
 
-        <div className="grid-cols-form grid gap-3 pt-5">
+        <div className="grid grid-cols-form gap-3 pt-5">
           <label
             htmlFor="country"
             className="text-sm font-medium text-zinc-700"
@@ -102,7 +110,7 @@ export function Details() {
           </Input.Root>
         </div>
 
-        <div className="grid-cols-form grid gap-3 pt-5">
+        <div className="grid grid-cols-form gap-3 pt-5">
           <label
             htmlFor="timezone"
             className="text-sm font-medium text-zinc-700"
@@ -115,7 +123,7 @@ export function Details() {
           </Input.Root>
         </div>
 
-        <div className="grid-cols-form grid gap-3 pt-5">
+        <div className="grid grid-cols-form gap-3 pt-5">
           <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
             Bio
             <span className="mt-0.5 block text-sm font-normal text-zinc-500">
@@ -126,7 +134,7 @@ export function Details() {
           <div></div>
         </div>
 
-        <div className="grid-cols-form grid gap-3 pt-5">
+        <div className="grid grid-cols-form gap-3 pt-5">
           <label
             htmlFor="projects"
             className="text-sm font-medium text-zinc-700"
@@ -137,7 +145,11 @@ export function Details() {
             </span>
           </label>
 
-          <div></div>
+          <FileInput.Root>
+            <FileInput.Trigger />
+            <FileInput.FileList />
+            <FileInput.Control multiple />
+          </FileInput.Root>
         </div>
 
         <div className="flex items-center justify-end gap-2 pt-5">
