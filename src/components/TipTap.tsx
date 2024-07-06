3ceded7c2c1857c6adc3f681138571bef2bc5dd0
Emerson Mendonça <emerson_10_s@hotmail.com>
2024-07-06 13:38:30 -0300

    feat: implementação (link,list,list-orderned) no editor

--- src/components/TipTap.tsx
+++ src/components/TipTap.tsx
@@ -5,21 +5,37 @@ import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import Italic from '@tiptap/extension-italic'
+import ListItem from '@tiptap/extension-list-item'
+import OrderedList from '@tiptap/extension-ordered-list'
+import BulletList from '@tiptap/extension-bullet-list'
+import Link from '@tiptap/extension-link'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import {
   Bold as BoldIcon,
   Italic as ItalicIcon,
-  Link,
+  Link as LinkIcon,
   List,
   ListOrdered,
 } from 'lucide-react'
 import { Select } from './Form/Select/Index'
 import { SelectItem } from './Form/Select/SelectItem'
+import { useCallback } from 'react'
 
 export function TipTap() {
   const editor = useEditor({
-    extensions: [Document, Paragraph, Text, StarterKit, Bold, Italic],
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      StarterKit,
+      Bold,
+      Italic,
+      ListItem,
+      OrderedList,
+      BulletList,
+      Link,
+    ],
     content: `
         <p>This isn’t bold.</p>
         <p><strong>This is bold.</strong></p>
@@ -30,6 +46,31 @@ export function TipTap() {
         <p style="font-weight: 999">Up to font weight 999!!!</p>
       `,
   })
+
+  const setLink = useCallback(() => {
+    const previousUrl = editor?.getAttributes('link').href
+    const url = window.prompt('URL', previousUrl)
+
+    // cancelled
+    if (url === null) {
+      return
+    }
+
+    // empty
+    if (url === '') {
+      editor?.chain().focus().extendMarkRange('link').unsetLink().run()
+
+      return
+    }
+
+    // update link
+    editor
+      ?.chain()
+      .focus()
+      .extendMarkRange('link')
+      .setLink({ href: url, target: '_blank' })
+      .run()
+  }, [editor])
 
   if (!editor) {
     return null
@@ -47,7 +88,7 @@ export function TipTap() {
           <button
             data-active={editor.isActive('bold')}
             type="button"
-            className="rounded-md p-2 hover:bg-zinc-50 data-[active='true']:bg-violet-900"
+            className="cursor-pointer rounded-md p-2 hover:bg-zinc-50 data-[active='true']:bg-violet-900"
           >
             <BoldIcon
               data-active={editor.isActive('bold')}
@@ -60,7 +101,7 @@ export function TipTap() {
           <button
             data-active={editor.isActive('italic')}
             type="button"
-            className="rounded-md p-2 hover:bg-zinc-50 data-[active='true']:bg-violet-900"
+            className="cursor-pointer rounded-md p-2 hover:bg-zinc-50 data-[active='true']:bg-violet-900"
           >
             <ItalicIcon
               data-active={editor.isActive('italic')}
@@ -70,23 +111,50 @@ export function TipTap() {
             />
           </button>
 
-          <button type="button" className="rounded-md p-2 hover:bg-zinc-50">
-            <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+          <button
+            data-active={editor.isActive('link')}
+            type="button"
+            className="cursor-pointer rounded-md p-2 hover:bg-zinc-50 data-[active='true']:bg-violet-900"
+          >
+            <LinkIcon
+              data-active={editor.isActive('link')}
+              onClick={setLink}
+              className="h-4 w-4 text-zinc-500 data-[active='true']:text-white"
+              strokeWidth={3}
+            />
           </button>
 
-          <button type="button" className="rounded-md p-2 hover:bg-zinc-50">
-            <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+          <button
+            data-active={editor.isActive('bulletList')}
+            type="button"
+            className="cursor-pointer rounded-md p-2 hover:bg-zinc-50 data-[active='true']:bg-violet-900"
+          >
+            <List
+              data-active={editor.isActive('bulletList')}
+              onClick={() => editor.chain().focus().toggleBulletList().run()}
+              className="h-4 w-4 text-zinc-500 data-[active='true']:text-white"
+              strokeWidth={3}
+            />
           </button>
 
-          <button type="button" className="rounded-md p-2 hover:bg-zinc-50">
-            <ListOrdered className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+          <button
+            data-active={editor.isActive('orderedList')}
+            type="button"
+            className="cursor-pointer rounded-md p-2 hover:bg-zinc-50 data-[active='true']:bg-violet-900"
+          >
+            <ListOrdered
+              data-active={editor.isActive('orderedList')}
+              onClick={() => editor.chain().focus().toggleOrderedList().run()}
+              className="h-4 w-4 text-zinc-500 data-[active='true']:text-white"
+              strokeWidth={3}
+            />
           </button>
         </div>
       </div>
 
       <EditorContent
         editor={editor}
-        className="rounded-lg border border-zinc-300 p-4 focus:border-violet-600 focus:outline-none focus:ring-2 focus:ring-violet-600"
+        className="prose rounded-lg border border-zinc-300 p-4 focus:border-violet-600 focus:outline-none focus:ring-2 focus:ring-violet-600"
       />
     </div>
   )
