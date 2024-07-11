a5925b32bfcf725353e97d7b54fb6bae56054dd9
Emerson Mendonça <emerson_10_s@hotmail.com>
2024-07-10 22:56:29 -0300

    feat:(component/button)  adicionando variants

--- src/components/SettingsTabs/Tabs/Datatils.tsx
+++ src/components/SettingsTabs/Tabs/Datatils.tsx
@@ -5,6 +5,7 @@ import { Mail } from 'lucide-react'
 import * as FileInput from '@/components/Form/FileInput'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
 import { TipTap } from '@/components/TipTap'
+import { Button } from '@/components/Button'
 
 export function Details() {
   return (
@@ -17,19 +18,12 @@ export function Details() {
           </span>
         </div>
         <div className="flex items-center gap-2">
-          <button
-            type="button"
-            className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-          >
+          <Button variant="outline" type="button">
             Cancel
-          </button>
-          <button
-            type="submit"
-            form="settings"
-            className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-          >
+          </Button>
+          <Button type="submit" form="settings">
             Save
-          </button>
+          </Button>
         </div>
       </div>
       <Separator />
@@ -159,18 +153,10 @@ export function Details() {
         </div>
 
         <div className="flex items-center justify-end gap-2 pt-5">
-          <button
-            type="button"
-            className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-          >
+          <Button variant="outline" type="button">
             Cancel
-          </button>
-          <button
-            type="submit"
-            className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-          >
-            Save
-          </button>
+          </Button>
+          <Button type="submit">Save</Button>
         </div>
       </form>
     </>
